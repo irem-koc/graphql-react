43905f9b114714d96221734d20eaec6cc180e97f
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-02-12 18:59:30 +0300

    fix: some errors related with context

--- src/context/Context.ts
+++ src/context/Context.ts
@@ -1,4 +1,15 @@
 import { createContext } from "react";
 import { ContextType } from "../type/ContextType";
 
-export const Context = createContext<ContextType | null>(null);
+export const Context = createContext<ContextType>({
+  filterSearch: "",
+  setFilterSearch: () => {},
+  countries: [],
+  setCountries: () => {},
+  group: "Name",
+  setGroup: () => {},
+  pageItem: 10,
+  setPageItem: () => {},
+  currentPage: 1,
+  setCurrentPage: () => {},
+});
